9d8ab9ba353e6238d71a9017bca95af35ef8d98a
Gheorghe Bulicanu <gicu@live.com>
2020-06-29 14:52:42 +0300

    Add counter view

--- flashcards.py
+++ flashcards.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+counter = 0
+
 
 @app.route("/")
 def welcome():
@@ -12,3 +14,10 @@ def welcome():
 @app.route("/date")
 def date():
     return f"This page was served at {datetime.now()}"
+
+
+@app.route("/counter")
+def counter_view():
+    global counter
+    counter += 1
+    return f"This page was viewed {counter} times."
