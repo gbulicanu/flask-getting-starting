858395a6e7534c5343e711ac8cf4433b5ba8c646
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-07-05 17:40:05 +0300

    Add APIs in flask app

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, jsonify
 from model import db
 
 app = Flask(__name__)
@@ -22,5 +22,18 @@ def card_view(index):
                                card=card,
                                index=index,
                                max_index=len(db)-1)
+    except IndexError:
+        abort(404)
+
+
+@app.route("/api/card/")
+def api_card_list():
+    return jsonify(db)
+
+
+@app.route('/api/card/<int:index>')
+def api_card_detail(index):
+    try:
+        return db[index]
     except IndexError:
         abort(404)
