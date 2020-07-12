706da16efdcb8ae31ce9e2a69caacc6ff0ab908e
Gheorghe Bulicanu <gicu@live.com>
2020-07-12 15:56:43 +0300

    Add encoding

--- model.py
+++ model.py
@@ -12,12 +12,12 @@ import json
 
 
 def load_db():
-    with open("flashcards_db.json") as f:
+    with open("flashcards_db.json", encoding="utf-8-sig") as f:
         return json.load(f)
 
 
 def save_db():
-    with open("flashcards_db.json", 'w') as f:
+    with open("flashcards_db.json", 'w', encoding="utf-8-sig") as f:
         return json.dump(db, f)
 
 
